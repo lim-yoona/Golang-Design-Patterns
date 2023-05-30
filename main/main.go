2aade1a726ea5f651c828b51318b4ba12257adb1
yoonamessi123 <1994238842@qq.com>
2023-05-30 11:27:59 +0800

    update main.go

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import "Golang-Design-Patterns/principle"
 
 // main.go用于测试代码
 func main() {
+	// 测试principle/开闭原则.go中的多态
 	sb := principle.SaveBank{}
 	tb := principle.TranBank{}
 	gb := principle.GetMoneyBank{}
